4999f43fde554ae913cac34f4c03a2ded0459578
KBMSr <kryvenko_bogdan@ukr.net>
2016-03-25 17:41:15 +0200

    initial commit

---
+++ JustBlog.Core/Mappings/CategoryMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using JustBlog.Core.Mappings;
+
+namespace JustBlog.Core.Mappings
+{
+    public class CategoryMap: ClassMap<Category>
+    {
+        public CategoryMap ()
+        {
+
+        }
+    }
+}
---
+++ JustBloge/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JustBloge.MvcApplication" Language="C#" %>
